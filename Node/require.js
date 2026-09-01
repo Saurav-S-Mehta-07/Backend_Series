c0185c00e4eca99444d319f8fbf29c276d08afb4
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-01 21:00:07 +0530

    directory name changed

--- Node/require.js
+++
@@ -1,9 +0,0 @@
-// const math = require("./math");
-// console.log(math.sum(4,5));
-
-// const math = require("./math");
-// console.log(math);
-
-
-const {sum, product, g, PI} = require("./math");
-console.log(sum(4,5));
