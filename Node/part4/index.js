6f6fd960196978ad219c2ec320ec07a0ec02fe62
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-01 20:50:47 +0530

    give a joke created

---
+++ Node/part4/index.js
@@ -0,0 +1,28 @@
+let giveMeAJoke = require('give-me-a-joke');
+
+// To get a random dad joke
+giveMeAJoke.getRandomDadJoke (function(joke) {
+    console.log(joke);
+});
+
+
+// // To get a random Chuck Norris joke
+// giveMeAJoke.getRandomCNJoke (function(joke) {
+//     console.log(joke);
+// });
+
+// // To get a customized joke
+// let fn = "Jackie";
+// let ln = "Chan";
+// giveMeAJoke.getCustomJoke (fn, ln, function(joke) {
+//     console.log(joke);
+// });
+
+// // To get a random Joke of the Day (Categories allowed: "blonde", "knock-knock", "animal", "jod")
+// let category = "blonde";
+// giveMeAJoke.getRandomJokeOfTheDay (category, function(joke) {
+//      // NOTE: The service provider has made the restriction of 5 calls an hour for RandomJokeOfTheDay
+//     console.log(joke);
+// });
+
+giveMeAJoke.getRandomCNJoke;
