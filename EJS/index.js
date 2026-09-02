477c711ebe984aff47bca57e51330644e020a4e3
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-02 17:14:23 +0530

    setting views folder

--- EJS/index.js
+++ EJS/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/",(req,res)=>{
     // res.send("Home page");
     res.render("home.ejs"); // res.render("home")
+})
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello");
 })
 
 
