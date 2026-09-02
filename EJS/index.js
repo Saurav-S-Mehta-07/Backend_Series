d85d6145ee6753041a2990d96e6934ffcd7aae59
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-02 17:03:40 +0530

    rendering home page

---
+++ EJS/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/",(req,res)=>{
+    // res.send("Home page");
+    res.render("home.ejs");
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Listening to port ${PORT}`);
+})
