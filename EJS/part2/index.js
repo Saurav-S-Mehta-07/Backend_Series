f6ab01fa79c5874ae42f301a76dd8cbd8dd4eff3
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-02 17:38:09 +0530

    /ig/:username route created

---
+++ EJS/part2/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app  = express();
+const PORT = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+
+app.get("/", (req, res)=>{
+    res.send("HOME PAGE");
+})
+
+app.get("/ig/:username",(req,res)=>{
+    let {username} = req.params;
+    res.render("insta", {username});
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening to PORT ${PORT}`);
+})
