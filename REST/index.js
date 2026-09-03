361d2e5202267a434c8bf116f7f5629b0e5da7b3
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-03 20:37:53 +0530

    edit and delete route

--- REST/index.js
+++ REST/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const path = require("path");
+const methodOverride = require('method-override')
 
 const { v4: uuidv4 } = require('uuid');
 
@@ -12,6 +13,8 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+app.use(methodOverride('_method'));
 
 let posts =[
     {
@@ -45,7 +48,8 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let {username, content} = req.body;
-    posts.push({id : uuidv4(), username, content});
+    let id  = uuidv4();
+    posts.push({id, username, content});
     res.redirect("/posts");
 })
 
@@ -53,6 +57,26 @@ app.get("/posts/:id",(req,res)=>{
     let {id} = req.params;
     let post = posts.find((post)=> id === post.id);
     res.render("show.ejs", {post});
+})
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((post)=> id === post.id);
+    post.content = newContent;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((post)=> id === post.id);
+    res.render("edit.ejs",{post});
+})
+
+app.delete("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((post)=>id!==post.id);
+    res.redirect("/posts");
 })
 
 app.listen(PORT, ()=>{
