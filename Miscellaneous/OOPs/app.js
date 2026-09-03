853fe7d01f1111a3de847008bca7984e03d1e6e5
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-03 18:07:47 +0530

    factory function + constructor

--- Miscellaneous/OOPs/app.js
+++ Miscellaneous/OOPs/app.js
@@ -1,7 +1,42 @@
-let arr = [1,2,3];
-let arr2 = [4,5,6];
+// object prototype
+
+// let arr = [1,2,3];
+// let arr2 = [4,5,6];
 
 // arr.sayHello = ()=>(console.log("Hello!, I'm ARR"));
 // arr2.sayHello = ()=>(console.log("Hello!, I'm ARR"));
 
-Array.prototype.sayHello = ()=>(console.log("Hello!"));
+// Array.prototype.sayHello = ()=>(console.log("Hello!"));
+
+
+//factory functions
+
+// function PersonMaker(name, age){
+//      const person = {
+//         name : name,
+//         age : age,
+//         talk(){
+//             console.log(`Hi!, my name is ${this.name}`);
+//         }
+//      }
+//      return person;
+// }
+
+// let p1 = PersonMaker("Saurav", 20);
+// let p2 = PersonMaker("Shivam", 21);
+// console.log(p1.talk == p2.talk) // false
+
+// Constructors
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.talk = function(){
+    console.log(`Hi! My name is ${this.name}`);
+}
+
+let p1 = new Person("saurav", 20);
+let p2 = new Person("shivam", 21);
+// console.log(p1.talk == p2.talk) // true
+
