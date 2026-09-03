d51940bfc2d4528fd8f6649a9a5ac447fdb1e60a
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-03 19:43:57 +0530

    show route created

--- REST/index.js
+++ REST/index.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 8080;
 const path = require("path");
 
+const { v4: uuidv4 } = require('uuid');
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -13,14 +15,17 @@ app.use(express.json());
 
 let posts =[
     {
+        id : "1a",
         username : "saurav",
         content : "I love coding"
     },
     {
+        id : "2b",
         username : "mayank",
         content : "smartwork is important to achieve success"
     },
     {
+        id : "3c",
         username : "shivam",
         content : "Internship is going well!"
     }
@@ -40,8 +45,14 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
     let {username, content} = req.body;
-    posts.push({username, content});
+    posts.push({id : uuidv4(), username, content});
     res.redirect("/posts");
+})
+
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((post)=> id === post.id);
+    res.render("show.ejs", {post});
 })
 
 app.listen(PORT, ()=>{
