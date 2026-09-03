a7b5e3b29e2bf59684fc3470e3dd08218c33ff92
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-03 17:31:04 +0530

    student objects

---
+++ Miscellaneous/OOPs/app.js
@@ -0,0 +1,17 @@
+const std1 = {
+    name : "adam",
+    age : 23,
+    marks : 94,
+    getMarks : function(){
+        return this.marks;
+    }
+};
+
+const std2 = {
+    name : "bob",
+    age : 25,
+    marks : 92,
+    getMarks : function(){
+        return this.marks;
+    }
+};
