41d7ddc51b5cb0153e0bdda22a80e95e8ba6666e
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-04 16:27:57 +0530

    insert data into db + model

--- MongoDB/Mongoose/index.js
+++ MongoDB/Mongoose/index.js
@@ -4,6 +4,32 @@ main()
 .then(() => console.log('MongoDB Connected!'))
 .catch((err)=>console.log(err));
 
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name:String,
+    email:String,
+    age:Number,
+});
+
+
+const User = mongoose.model("User", userSchema); // collection name : users
+
+// const user1 = new User({name:"Adam", email:"adam@gmail.com", age:20});
+// user1.save();
+
+const user2 = new User({name:"Eve", email:"eve@gmail.com", age:20});
+
+user2.save() // returns promise
+.then((res)=>{
+    console.log("user inserted to the collection : ", res);
+})
+.catch((err)=>{
+    console.log("haha! I found an Error & that is : ", err);
+})
+
+
 async function main() {
-   await mongoose.connect('mongodb://127.0.0.1:27017/test')
+   await mongoose.connect('mongodb://127.0.0.1:27017/test') // returns promise
 }
