18aef76a8ed40e4a37ec2dba530f4b33d06d5715
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-08 15:23:21 +0530

    our own express error class for errors

--- Errors/app.js
+++ Errors/app.js
@@ -1,30 +1,36 @@
 const express = require("express");
-
 const app = express();
+const ExpressError = require("./ExpressError");
+
+
+app.use("/api",(req,res,next)=>{
+    let {token} = req.query;
+    if(token === "access123"){
+        return next();
+    }
+    throw new ExpressError(401, "Access Denied");
+})
 
 app.get("/", (req, res) => {
     res.send("home route");
 });
 
+app.get("/api",(req,res)=>{
+    res.send("data")
+})
+
 app.get("/error", (req, res) => {
     abcd = abcd;
 });
 
-// Error-handling middleware MUST be last
 app.use((err, req, res, next) => {
     console.log("-----------------error----------");
-    next(err);
+    res.send(err);
 });
 
-
-app.use((err,req,res,next)=>{
-    console.log("-------2nd error handler------");
-    next(err);
+app.use((req,res)=>{
+    res.send("page not found")
 })
-
-// app.use((req,res)=>{
-//     res.send("page not found")
-// })
 
 app.listen(8080, () => {
     console.log("Listening to port 8080");
