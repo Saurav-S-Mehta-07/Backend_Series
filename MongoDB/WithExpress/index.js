ee0932425a61e69281c95048a5cf3bc70b6ae979
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-08 16:51:53 +0530

    error handling middleware learned

--- MongoDB/WithExpress/index.js
+++ MongoDB/WithExpress/index.js
@@ -17,6 +17,9 @@ app.use(methodOverride("_method"))
 const Chat = require("./models/Chat.js")
 const { resourceLimits } = require("worker_threads")
 
+const ExpressError = require("./ExpressError.js");
+const asyncWrap = require("./WrapAsync.js");
+
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp')
@@ -27,55 +30,105 @@ app.get("/",(req,res)=>{
     res.redirect('/chats')
 })
 
-app.get("/chats",async(req,res)=>{
+app.get("/chats",asyncWrap(async(req,res)=>{
     let chats = await Chat.find();
     let date = chats[0].created_at
     res.render("index",{chats})
-})
+}))
 
 app.get("/chats/new",(req,res)=>{
+    // throw new ExpressError(404, "Page not found") //trying handler in non-async function
     res.render("new")
 })
 
-app.post("/chats",async(req,res)=>{
+app.post("/chats",asyncWrap(async(req,res,next)=>{
     let {from, to, msg} = req.body;
     let created_at = new Date();
     let newChat = {from:from, to:to, msg:msg, created_at:created_at}
     let result = await Chat.insertOne(newChat)
     res.redirect("/chats")
-})
+}))
 
-
-app.get("/chats/:id/edit", async(req,res)=>{
+app.get("/chats/:id/edit", asyncWrap(async(req,res)=>{
     let {id} = req.params
     let chat = await Chat.findById(id)
-    console.log(chat)
     res.render("edit", {chat})
-})
+}))
 
-app.get("/chats/:id",async(req,res)=>{
+
+
+//show route
+// app.get("/chats/:id",async(req,res,next)=>{
+//         let {id} = req.params;
+//         let chat = await Chat.findById(id)
+//         // if(!chat){
+//         //     throw new ExpressError(404, "chat not found") //trying handler in async function -> but express bydefault not call next
+//         // }
+//         if (!chat){ //even try but chat can be undefine if cast error id long then try will handle
+//           return  next(new ExpressError(404, "Chat not found")) // explicitly call next and now handler work
+//         }
+//         res.render("show.ejs",{chat})
+// })
+
+// app.get("/chats/:id",async(req,res,next)=>{
+//     try{
+//         let {id} = req.params;
+//         let chat = await Chat.findById(id)
+//         if (!chat){ 
+//           return  next(new ExpressError(404, "Chat not found"))
+//         }
+//         res.render("show.ejs",{chat})
+//     }
+//     catch(err){
+//         next(err)
+//     }
+// })
+
+app.get("/chats/:id",asyncWrap(async(req,res,next)=>{
     let {id} = req.params;
     let chat = await Chat.findById(id)
+    if (!chat){ 
+        return  next(new ExpressError(404, "Chat not found")) 
+    }
     res.render("show.ejs",{chat})
-})
+}))
 
-app.patch("/chats/:id",async(req,res)=>{
+app.patch("/chats/:id",asyncWrap(async(req,res)=>{
     let {id} = req.params
     let {msg} = req.body;
     let updatedChat = {msg:msg, created_at: new Date()}
     let result = await Chat.findByIdAndUpdate(id, updatedChat, {runValidators:true})
     res.redirect("/chats")
-})
+}))
 
 
-
-app.delete("/chats/:id", async(req,res)=>{
+app.delete("/chats/:id", asyncWrap(async(req,res)=>{
     let {id} = req.params
-
     let result = await Chat.findByIdAndDelete(id);
-    console.log(result, "this chat is deleted successfully")
     res.redirect("/chats")
+}))
+
+
+const handleValidationErr = (err)=>{
+    console.log("this was a validation error please follow rules");
+    console.log(err.message);
+    return err;
+}
+
+// error handling middleware
+
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if(err.name == "ValidationError"){
+      err =  handleValidationErr(err)
+    }
+    next(err)
 })
+app.use((err,req,res,next)=>{
+    let {status=500, message="some error occurred"} = err;
+    res.status(status).send(message);
+})
+
 
 const connectToDB = ()=>{
     main()
@@ -86,6 +139,7 @@ const connectToDB = ()=>{
         console.log("Error  : ", err.message)
     })
 }
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening to PORT ${PORT}`)
