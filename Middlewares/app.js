8d1bc873f7e51d5f0b6e6d5abc3fb7c31fbb2072
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-08 14:26:31 +0530

    storing middleware in variable and multiple middleware

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -43,15 +43,51 @@ const app = express();
 
 
 // this middleware run for all routes as previous
-app.use("/",(req,res,next)=>{
-    console.log("I'm only / route but I'll run for all")
-    next()
-})
+// app.use("/",(req,res,next)=>{
+//     console.log("I'm only / route but I'll run for all")
+//     next()
+// })
 
-// this middleware for specific path /random and /random/anything
-app.use("/random",(req,res,next)=>{
-    console.log("I'm only for random")
-    next()
+// // this middleware for specific path /random and /random/anything
+// app.use("/random",(req,res,next)=>{
+//     console.log("I'm only for random")
+//     next()
+// })
+
+
+// one way to write to middleware
+// app.use("/api",(req,res,next)=>{
+//     let {token} = req.query
+//     if(token==="access123"){
+//        return next();
+//     }
+//     res.send("access denied");
+// })
+
+// app.get("/api",checkToken,(req,res)=>{
+//     res.send("welcome! these APIs is for free") 
+// })
+
+
+// second way to write middleware
+const checkToken = (req,res,next)=>{
+    let {token} = req.query
+    if(token==="access123"){
+       return next();
+    }
+    res.send("access denied");
+}
+
+const isLoggedIn = (req,res,next)=>{
+    let {login} = req.query
+    if(login==="saurav"){
+       return next();
+    }
+    res.send("access denied");
+}
+
+app.get("/api",checkToken,isLoggedIn,(req,res)=>{
+    res.send("welcome! these APIs is for free") 
 })
 
 app.get("/",(req,res)=>{
@@ -61,6 +97,10 @@ app.get("/",(req,res)=>{
 app.get("/random",(req,res)=>{
     res.send("This is a random page")
 })
+
+app.use((req,res)=>{
+    res.status(404).send("Page not found");
+});
 
 
 app.listen(8080, ()=>{
