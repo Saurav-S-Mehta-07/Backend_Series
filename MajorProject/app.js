336d8a34bbf9e8b8d0bbd098988ff4c00906277c
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-09 18:41:21 +0530

    server side error handling done

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -18,6 +18,9 @@ app.engine("ejs", ejsMate)
 
 const Listing = require("./models/listing")
 
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js")
+
 
 // connection to DB
 const mongodb_url = 'mongodb://127.0.0.1:27017/explora'
@@ -37,10 +40,10 @@ app.get("/",(req,res)=>{
 
 
 // Index Route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     let allListings = await Listing.find()
     res.render("listings/index.ejs",{allListings})
-})
+}))
 
 
 // New Route
@@ -49,40 +52,58 @@ app.get("/listings/new",(req,res)=>{
 })
 
 //Create Route
-app.post("/listings",async(req,res)=>{
+app.post("/listings",wrapAsync(async(req,res,next)=>{
     let listing = req.body.listing
-    await Listing.insertOne(listing)
+    if(!req.body.listing){
+       throw new ExpressError(400, "Send valid data for listing")
+    }
+    let newListing = new Listing(listing)
+    await newListing.save()
     res.redirect("/listings")
-})
+}))
 
 // Show Route
-app.get("/listings/:id", async(req,res)=>{
+app.get("/listings/:id",wrapAsync( async(req,res)=>{
     let {id} = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-})
+}))
 
 
 //edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id} = req.params
     let listing = await Listing.findById(id)
     res.render("listings/edit.ejs",{listing})
-})
+}))
 
 // update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params
     let listing = req.body.listing
+    if(!listing){
+        throw new ExpressError(400, "Send valid data for listing")
+    }
     await Listing.findByIdAndUpdate(id, listing, {runValidators : true})
     res.redirect(`/listings/${id}`);
-})
+}))
 
 // destroy route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params
     await Listing.findByIdAndDelete(id)
     res.redirect("/listings")
+}))
+
+
+app.use((req,res,next)=>{
+    next(new ExpressError(404, "Page not Found"))
+})
+
+app.use((err,req,res,next)=>{
+    let {status = 500, message = "something went wrong"} = err
+    res.status(status).render("error.ejs",{err});
+    // res.status(status).send(message)
 })
 
 app.listen(PORT,()=>{
