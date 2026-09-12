0b3e80017219705b8e80c16af07fae26b2a97719
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-12 12:35:00 +0530

    review model + handle deletion functionality created

--- MajorProject/models/listing.js
+++ MajorProject/models/listing.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
+const Review = require("./review.js");
+
 const listingSchema = Schema({
     title:{
       type : String,
@@ -28,9 +30,21 @@ const listingSchema = Schema({
     country : {
         type: String,
         required:true
-    }
+    },
+
+    reviews : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Review"
+        }
+    ]
+})
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+   if(listing){
+     await Review.deleteMany({_id : {$in : listing.reviews}})
+   }
 })
 
 const Listing = mongoose.model("Listing", listingSchema)
-
 module.exports = Listing
