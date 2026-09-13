4cffb6c05a22d4d859ef565a5538f50de082849f
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-13 13:12:55 +0530

    user login post route created

--- MajorProject/routes/user.js
+++ MajorProject/routes/user.js
@@ -1,35 +1,51 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 
-router.get("/signup", (req,res)=>{
-    res.render("users/signup");
-})
+const passport = require("passport");
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{
-        let {username, email, password} = req.body;
-        const newUser = new User({email, username});
+router.get("/signup", (req, res) => {
+  res.render("users/signup");
+});
 
-        const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.flash("success","Welcome to wanderlust");
-        res.redirect("/listings")
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to wanderlust");
+      res.redirect("/listings");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
     }
-    catch(err){
-       req.flash("error",err.message);
-       res.redirect("/signup");
-    }
-}));
+  }),
+);
 
-router.get("/login",(req,res)=>{
-    res.render("users/login");
-})
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
 
-router.get("/logout", (req,res)=>{
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to Explora!")
     res.redirect("/listings");
-})
+  },
+);
 
-module.exports = router
+router.get("/logout", (req, res) => {
+  res.redirect("/listings");
+});
+
+module.exports = router;
