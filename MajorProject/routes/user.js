323b74471a33008655b3473cd748e4f167179ba9
Saurav s Mehta <sauravmehta631@gmail.com>
2026-09-13 13:45:28 +0530

    auto login after signup

--- MajorProject/routes/user.js
+++ MajorProject/routes/user.js
@@ -12,15 +12,20 @@ router.get("/signup", (req, res) => {
 
 router.post(
   "/signup",
-  wrapAsync(async (req, res) => {
+  wrapAsync(async (req, res,next) => {
     try {
       let { username, email, password } = req.body;
       const newUser = new User({ email, username });
 
       const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.flash("success", "Welcome to wanderlust");
-      res.redirect("/listings");
+      
+      req.login(registeredUser,(err)=>{
+             if(err){
+               return next(err);
+             }
+             req.flash("success","Welcome to Explora");
+             res.redirect("/listings");
+      });
     } catch (err) {
       req.flash("error", err.message);
       res.redirect("/signup");
